96c7dce540e436f52a2b1f676a97177f708630a7
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-01-28 16:01:08 +0530

    Home page updated and LocalSearchPanel work

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const LocalSearchPanel = ()=>{
+    return(
+        <div>
+           {/* This is a temporary database */}
+
+           <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-10 flex items-center justify-center w-12 rounded-full"><i class="ri-map-pin-line"></i></h2>
+            <h4 className="font-medium">24B, Near Kapoor's cafe, Shreiyans Coding School, Bhopal</h4>
+           </div>
+           
+           <div className="flex gap-4 items-center my-2 justify-start">
+            <h2 className="bg-[#eee] h-10 flex items-center justify-center w-12 rounded-full"><i class="ri-map-pin-line"></i></h2>
+            <h4 className="font-medium">24B, Near Kapoor's cafe, Shreiyans Coding School, Bhopal</h4>
+           </div>
+
+           <div className="flex gap-4 items-center my-2 justify-start">
+            <h2 className="bg-[#eee] h-10 flex items-center justify-center w-12 rounded-full"><i class="ri-map-pin-line"></i></h2>
+            <h4 className="font-medium">24B, Near Kapoor's cafe, Shreiyans Coding School, Bhopal</h4>
+           </div>
+
+           <div className="flex gap-4 items-center my-2 justify-start">
+            <h2 className="bg-[#eee] h-10 flex items-center justify-center w-12 rounded-full"><i class="ri-map-pin-line"></i></h2>
+            <h4 className="font-medium">24B, Near Kapoor's cafe, Shreiyans Coding School, Bhopal</h4>
+           </div>
+           
+
+          
+        </div>
+    )
+}
+
+export default LocalSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,29 +2,42 @@ import React, { useRef, useState } from "react";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 import 'remixicon/fonts/remixicon.css'
-
-
+import LocalSearchPanel from "../components/LocationSearchPanel";
 const Home = () => {
     const[pickup , setPickup] =useState('');
     const [destination , setDestination] = useState('');
     const [panelOpen , setPanelOpen] = useState(false);
     const panelRef = useRef(null)
+    const panelCloseRef = useRef(null);
     const submitHandler = (e)=>{
         e.preventDefault();
     }
     useGSAP(function(){
         if(panelOpen){
             gsap.to(panelRef.current,{
-                height:'70%'
+                height:'70%',
+                padding : 24,
+                opacity:1
             })
+            gsap.to(panelCloseRef.current,{
+                opacity:1
+            })
+
         }else{
-            height:'0%'
+            gsap.to(panelRef.current,{
+                height:'0%',
+                padding:24,
+                opacity : 0
+            })
+            gsap.to(panelCloseRef.current,{
+                opacity:0
+            })
         }
     },[panelOpen])
 
 
     return (
-        <div className="h-screen relative">
+        <div className="h-screen relative overflow-hidden">
             <img 
                 className="w-16 absolute left-5 top-5" 
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3yF-nFzNZSoDYqYDahDHQf5p2ttS3viGK3g&s"/>
@@ -34,10 +47,10 @@ const Home = () => {
             </div>
             <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
                 <div className="h-[30%] bg-white p-5 relative">
-                    <h5 onClick={()=>{
+                    <h5 ref={panelCloseRef}onClick={()=>{
                         setPanelOpen(false)
 
-                    }}className="absolute right-2 top-6 text-2xl">
+                    }}className="absolute opacity-0 right-2 top-6 text-2xl">
                     <i class="ri-arrow-down-wide-line"></i>
                     </h5>
                     <h4 className="text-2xl font-semibold">Find a trip</h4>
@@ -73,9 +86,24 @@ const Home = () => {
                         />
                     </form>
                 </div>
-                <div ref={panelRef}className="bg-red-500 h-0">
-
+                <div ref={panelRef}className="bg-white h-0 ">
+                    <LocalSearchPanel/>
                 </div>
+            </div>
+
+            <div className="fixed w-full z-10 bg-white bottom-0 px-3 py-6">
+                <h3 className="text-2xl font-semibold mb-3">Choose a Vehicle</h3>
+                <div className="flex border-2 border-black mb-2 rounded-xl w-full  flex items-center justify-between p-3">
+                    <img className="h-12" src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1555367310/assets/30/51e602-10bb-4e65-b122-e394d80a9c47/original/Final_UberX.png"/>
+                    <div className=" w-1/2">
+                        <h4 className="font-medium text-base">Uber Go <span><i class="ri-user-3-fill"></i>4</span></h4>
+                        <h5>2 mins away </h5>
+                        <p className="font-normal text-xs text-gray-600">Affordable, compact rides</p>
+                        
+                    </div>
+                    <h2 className="text-xl font-semibold">₹193.20</h2>
+                </div>
+
             </div>
         </div>
     )
